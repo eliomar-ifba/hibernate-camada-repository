52158691ad39e24294ef7b7359fd1a9d5969ee0c
Eliomar Campos - IFBA <eliomar.systems@gmail.com>
2025-03-07 17:08:01 -0300

    Update AlunoRepository.java

--- src/main/java/edu/ifba/repository/AlunoRepository.java
+++ src/main/java/edu/ifba/repository/AlunoRepository.java
@@ -8,10 +8,11 @@ import jakarta.persistence.EntityManagerFactory;
 
 public class AlunoRepository implements AlunoRepositoryInterface {
     private static EntityManagerFactory fabrica = FabricaConexao.getEntityManagerFactory();
-    private EntityManager em = fabrica.createEntityManager();
+    private EntityManager em;
 
     @Override
     public void salvar(Aluno aluno) {
+        em = fabrica.createEntityManager();
         em.getTransaction().begin();
         em.persist(aluno);
         em.getTransaction().commit();
@@ -20,6 +21,7 @@ public class AlunoRepository implements AlunoRepositoryInterface {
 
     @Override
     public void deletar(int id) {
+        em = fabrica.createEntityManager();
         em.getTransaction().begin();
         em.remove(em.find(Aluno.class, id));
         em.getTransaction().commit();
@@ -28,6 +30,7 @@ public class AlunoRepository implements AlunoRepositoryInterface {
 
     @Override
     public void atualizar(Aluno aluno) {
+        em = fabrica.createEntityManager();
         em.getTransaction().begin();
         em.merge(aluno);
         em.getTransaction().commit();
@@ -36,6 +39,7 @@ public class AlunoRepository implements AlunoRepositoryInterface {
 
     @Override
     public List<Aluno> buscarTodos() {
+        em = fabrica.createEntityManager();
         List<Aluno> alunos = em.createQuery("SELECT a FROM Aluno a", Aluno.class).getResultList();
         em.close();
 
@@ -44,6 +48,7 @@ public class AlunoRepository implements AlunoRepositoryInterface {
 
     @Override
     public Aluno buscarPorId(int id) {
+        em = fabrica.createEntityManager();
         Aluno alunoEncontrado = em.find(Aluno.class, id);
         em.close();
         return alunoEncontrado;
@@ -51,6 +56,7 @@ public class AlunoRepository implements AlunoRepositoryInterface {
 
     @Override
     public Aluno buscarPorMatricula(String matricula) {
+        em = fabrica.createEntityManager();
         Aluno alunoEncontrado = em.createQuery("SELECT a FROM Aluno a WHERE a.matricula = :matricula", Aluno.class)
                 .setParameter("matricula", matricula)
                 .getSingleResultOrNull();
@@ -61,6 +67,7 @@ public class AlunoRepository implements AlunoRepositoryInterface {
 
     @Override
     public List<Aluno> buscarPorNomeComLike(String nome) {
+        em = fabrica.createEntityManager();
         List<Aluno> alunos = em.createQuery("SELECT a FROM Aluno a WHERE a.nome LIKE :nome", Aluno.class)
                 .setParameter("nome", nome)
                 .getResultList();
